1262380fc3f41688ab040d3ca6b8bf573c3d9495
Mika Green <Damikag@gmail.com>
2020-05-25 19:02:12 -0400

    moved method

--- src/Homework/Bank/Account.java
+++ src/Homework/Bank/Account.java
@@ -10,30 +10,9 @@ public class Account  {
     double amount;
     int accountNumber;
     int accountAmount;
-    String[] customerAccountsType = new String[4];
-    Double[] customerAccountsAmount = new Double[4];
 
 
 
-    public  double makeDeposit (double amount) {
-        if (amount > 0) {
-            this.balance+=amount;
-            System.out.println("\nDeposit of: $" + amount + " was successful \nNew Balance: " + this.balance);
-        } else {
-            System.out.println("\nInvalid amount to deposit -> " + amount);
-        }
-        return this.balance;
-    }
-
-    public  double makeWithdraw (double amount) {
-        if (amount > 0 && amount <= balance) {
-            balance-=amount;
-            System.out.println("\nWithdraw of: $" + amount + " was successful \nNew Balance: " + balance);
-        } else {
-            System.out.println("\nInvalid amount to withdraw -> " + amount);
-        }
-        return balance;
-    }
 
     public void openNewAccount (String accType, double amount) {
         if (accountAmount != 5) {
--- src/Homework/Bank/Customer.java
+++ src/Homework/Bank/Customer.java
@@ -79,6 +79,26 @@ public class Customer extends Account{
             this.pinNumber = pinNumber;
         }
     }
+
+    public  double makeDeposit (double amount) {
+        if (amount > 0) {
+            this.balance+=amount;
+            System.out.println("\nDeposit of: $" + amount + " was successful \nNew Balance: " + this.balance);
+        } else {
+            System.out.println("\nInvalid amount to deposit -> " + amount);
+        }
+        return this.balance;
+    }
+
+    public  double makeWithdraw (double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance-=amount;
+            System.out.println("\nWithdraw of: $" + amount + " was successful \nNew Balance: " + balance);
+        } else {
+            System.out.println("\nInvalid amount to withdraw -> " + amount);
+        }
+        return balance;
+    }
 }
 
 
